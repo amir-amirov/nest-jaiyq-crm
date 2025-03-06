ae8cf3a0a08ac185017f962b7d81ef2982db2e2c
Amir Amirov <amir@Amirs-MacBook-Air.local>
2025-03-06 13:36:22 +0500

    Added PostgreSQL  and removed SQLite

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,15 +13,33 @@ import { User } from './users/user.entity';
 import { PaymentModule } from './payment/payment.module';
 import { RentalsModule } from './rentals/rentals.module';
 import { Rental } from './rentals/rental.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes config available everywhere
+    }),
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
+      type: 'postgres',
+      // host: process.env.DB_HOST,
+      // port: Number(process.env.DB_PORT),
+      // username: process.env.DB_USERNAME,
+      // password: process.env.DB_PASSWORD,
+      // database: process.env.DATABASE,
+      url: process.env.DATABASE_URL,
       entities: [User, Slot, Booking, Rental],
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false, // Needed for Railway and some cloud providers
+      },
     }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Slot, Booking, Rental],
+    //   synchronize: true,
+    // }),
     TypeOrmModule.forFeature([User, Slot, Booking, Rental]), // to be able to use User, Slot and Bookings repos in app
     UsersModule,
     SlotsModule,
